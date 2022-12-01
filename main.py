2e1f86de35126a03028c92ccca0a160ec574ea4e
Stead08 <inomatama08@gmail.com>
2022-12-01 22:47:18 +0900

    距離計算アプリ試作版 APIキー必須

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-from geopy.distance import geodesic
-import csv
+import googlemaps
+from datetime import datetime
 import pprint
-#飲食店の緯度経度リスト
-with open('Shop_location_list.csv') as s:
-    #Name,result,longitude,latitude
-    reader = csv.reader(s)
-    l_s = [row for row in reader]
-    print(l_s)
-#町の緯度経度リスト
-with open('Town_list.csv') as t:
-    # Address,Result,longitude,latitude,link for check
-    reader = csv.reader(t)
-    t_s = [row for row in reader]
-    print(t_s)
-#飲食店名
-restaurant_name = input("飲食店名:")
-#飲食店名の座標
-#restaurant_location = l_s.index(restaurant_name)
-#配達先
-deliver_point = input("配達先:")
 
-# (緯度, 経度)
-restaurant_location = (float(l_s[1][2]), float(l_s[1][3]))
-town_location = (float(t_s[1][2]), float(l_s[1][3]))
+gmaps = googlemaps.Client(key='AIzaSyBobOiyGLa9mIfp9oM15talEC5XLpZvQfQ')
 
-dis = geodesic(TokyoStation, NagoyaStation).km
+# Geocoding an address
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
 
-print(dis)
+# Look up an address with reverse geocoding
+#reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions((37.490857,139.926805),
+                                     (37.5027414,139.9500282),
+                                     departure_time=now)
+#directions_result = {'bounds': {'northeast': {'lat': 37.5028698, 'lng': 139.9517939}, 'southwest': {'lat': 37.48782449999999, 'lng': 139.9267699}}, 'copyrights': 'Map data ©2022', 'legs': [{'distance': {'text': '3.8 km', 'value': 3833}, 'duration': {'text': '9 mins', 'value': 526}, 'duration_in_traffic': {'text': '9 mins', 'value': 536}, 'end_address': '3-chōme-10-15 Iimori, Aizuwakamatsu, Fukushima 965-0007, Japan', 'end_location': {'lat': 37.5028698, 'lng': 139.9500554}, 'start_address': 'Japan, 〒965-0876 Fukushima, Aizuwakamatsu, Yamagamachi, 6−６７ サイトウBuild', 'start_location': {'lat': 37.490659, 'lng': 139.9267699}, 'steps': [{'distance': {'text': '3.7 km', 'value': 3661}, 'duration': {'text': '8 mins', 'value': 477}, 'end_location': {'lat': 37.5022748, 'lng': 139.9517776}, 'html_instructions': 'Head <b>east</b> toward <b>国道118号</b>/<wbr/><b>国道121号</b>', 'polyline': {'points': 'skycFim`uY@UDi@BUJ_B@SHyADi@Ba@XeFDo@F{@@c@FgA@KXwEFkAB_@LuBB_@LiBDcA@KBs@@OFgADo@BQ@UHoA@UBk@Bq@JaBBc@@ODq@Bi@@E?K@I?EBWBk@B[?KHoA@GHeA@MDs@@e@LiBDi@VaC?SVuC?EPsBv@kIPqBH}@b@uEFq@BYDe@@CDm@D_@Da@@O@KDg@BS@MBY@K@K?G?I@E?CAK?I?GAKCK?GCMAKCQACCSAOEQAKAGCQAI?AAEAM?CCKCOAECKEIIMKSIKOMIGIEIAC?CAE?C?C?S?A?c@@A?gABO@Y@sADA?aBDS@S@O?k@BC?[@i@BsABs@Ba@@K@U?oADU@M?sADc@BI?G?o@BsBFC?c@@a@@w@BO@]@U?Q?U@c@AUAYAUAOASEMAIA}@Oy@QI?YGs@OUEu@Ou@Mi@MIAo@M]GQEQC_AQk@K{@Qw@MsAWg@IKCa@IMEICIEOIIEWO[U{AgA_Aq@'}, 'start_location': {'lat': 37.490659, 'lng': 139.9267699}, 'travel_mode': 'DRIVING'}, {'distance': {'text': '0.2 km', 'value': 172}, 'duration': {'text': '1 min', 'value': 49}, 'end_location': {'lat': 37.5028698, 'lng': 139.9500554}, 'html_instructions': 'Turn <b>left</b><div style="font-size:0.9em">Destination will be on the left</div>', 'maneuver': 'turn-left', 'polyline': {'points': 'et{cFsieuYCASh@Yp@CDm@`BG`AEl@Ed@'}, 'start_location': {'lat': 37.5022748, 'lng': 139.9517776}, 'travel_mode': 'DRIVING'}], 'traffic_speed_entry': [], 'via_waypoint': []}], 'overview_polyline': {'points': 'skycFim`uYl@oJtAmVp@sLf@cJ`A}OVqE\\kDViDzAwPx@_Jb@}EJgABc@Aa@Im@WkBQqAUi@U_@YU[Ia@?oFNoQf@uIVgCHaB?uAGk@IqI}AqGmAoFaAeAW{@e@{DqC_BbEStC'}, 'summary': '', 'warnings': [], 'waypoint_order': []}
+pprint.pprint(directions_result[0]['legs'][0]['distance']['text'])
+
+# Validate an address with address validation
+#addressvalidation_result =  gmaps.addressvalidation(['1600 Amphitheatre Pk'],
+                                                    #regionCode='US',
+                                                    #locality='Mountain View',
+                                                    #enableUspsCass=True)
+#print(directions_result)
---
+++ sample007.py
@@ -0,0 +1,67 @@
+#https://www.salesanalytics.co.jp/datascience/datascience017/を参照して作成
+#ライブラリのインポート
+import streamlit as st
+import csv
+import requests
+# データの読み込み
+csv1 = "Shop_location_list.csv"
+shop_name_list = []
+with open(csv1) as c1:
+    reader = csv.reader(c1)
+    csvfile_header = next(reader)
+    # csvファイルのデータをループ
+    for row in reader:
+        # A列を配列へ格納
+        shop_name_list.append(str(row[0]))
+with open(csv1) as c1:
+    shop_list = list(csv.reader(c1))
+town_name_list = []
+csv2 = "Town_list.csv"
+with open(csv2) as c2:
+    reader = csv.reader(c2)
+    csvfile_header = next(reader)
+    # csvファイルのデータをループ
+    for row in reader:
+        # A列を配列へ格納
+        town_name_list.append(str(row[0]))
+with open(csv2) as c2:
+    town_list = list(csv.reader(c2))
+#タイトルを表示
+st.title('おみせごはんプロジェクト配達距離計算ツール')
+#飲食店リストを表示（ドロップダウン)
+st.subheader("飲食店を選択")
+shop_selected = st.selectbox("飲食店を一つ選択してください", shop_name_list)
+st.write(shop_selected, "が選択されました。")
+#町名リストを表示(ドロップダウン)
+st.subheader("配達先を選択")
+town_selected = st.selectbox("配達先を一つ選択してください", town_name_list)
+st.write(town_selected, "が選択されました。")
+#選択された飲食店と配達先の緯度経度を取得
+for i, line_s in enumerate(shop_list):
+    if shop_selected in line_s:
+        shop_selected_index = i
+shop_selected_longitude_and_latitude = shop_list[shop_selected_index][2], shop_list[shop_selected_index][3]
+for i, line_s in enumerate(town_list):
+    if town_selected in line_s:
+        town_selected_index = i
+town_selected_longitude_and_latitude = town_list[town_selected_index][2], town_list[town_selected_index][3]
+
+#直線距離を計算
+from geopy.distance import geodesic
+dis = geodesic(shop_selected_longitude_and_latitude, town_selected_longitude_and_latitude).km
+#Googleマップで車での移動距離を計算
+#google mapライブラリのインポート
+import googlemaps
+from datetime import datetime
+gmaps = googlemaps.Client(key='')
+now = datetime.now()
+directions_result = gmaps.directions(shop_selected_longitude_and_latitude,
+                                     town_selected_longitude_and_latitude,
+                                     departure_time=now)
+distance_driving = directions_result[0]['legs'][0]['distance']['text']
+#出力（テクスト）
+dis_round = round(dis, 2)
+st.subheader('直線距離')
+st.write('{}km'.format(dis_round))
+st.subheader('運転距離')
+st.write('{}'.format(distance_driving))
